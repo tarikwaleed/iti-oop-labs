d86fcba55260243a1e1e9b8a8142c91eaee914e9
tarikwaleed <takizee53@gmail.com>
2022-11-26 11:31:03 +0700

    function overloading

---
+++ assignment1/add.h
@@ -0,0 +1,6 @@
+int Add(int x=2,int y=3){
+    return x+y;
+}
+int Add(float x=2,float y=3){
+    return x+y;
+}
--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -1,18 +1,30 @@
 #include<iostream>
 #include "employee.h"
+#include "add.h"
 using namespace std;
+
 
 int main(int argc, char const *argv[])
 {
-    int size=2;
-    Employee* emps=  new Employee[size];
-    for(int i=0;i<size;i++){
-        emps[i].InitializeEmployee();
-    }
-    cout<<"The Data of All Employees"<<endl;
-    for(int j=0;j<size;j++){
-        emps[j].DisplayEmployee();
-    }
+    //Problem1
+
+    // int size=2;
+    // Employee* emps=  new Employee[size];
+    // for(int i=0;i<size;i++){
+    //     emps[i].InitializeEmployee();
+    // }
+    // cout<<"The Data of All Employees"<<endl;
+    // for(int j=0;j<size;j++){
+    //     emps[j].DisplayEmployee();
+    // }
+
+    //Problem2
+    cout<<Add(10,20)<<endl;
+
+    //Problem3
+    cout<<Add(10.2f,20.3f)<<endl;
+
+
 
 
     return 0;
